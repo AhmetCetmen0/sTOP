5d76c21ff97454d3806240c4921a39b9b87cfc66
AhmetCetmen0 <ahmetcetmen0@gmail.com>
2025-05-04 14:46:27 +0300

    restore

--- sTOP.py
+++ sTOP.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    disc = sheller.run("echo df -h")
-    cpu = sheller.run("lscpu")
+    # disc = sheller.run("echo df -h")
+    # cpu = sheller.run("lscpu")
     
-    gpu = sheller.run("nvidia-smi")
-    return render_template('index.html', disc=disc, cpu=cpu, gpu=gpu)
+    # gpu = sheller.run("nvidia-smi")
+    return render_template('index.html')#, disc=disc, cpu=cpu, gpu=gpu)
 @app.route('/ram')
 
 def ram_page():
-    ram = sheller.run("echo free -t -h --mega")
+    ram = sheller.run("free -t -h --mega")
     return render_template('ram.html', ram=ram)
 
 if __name__ == '__main__':
